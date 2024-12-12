ae00625318f8dfa71830103f57f699b20881898c
황혜인 <hhi9037@naver.com>
2024-12-12 21:26:51 +0900

    [feat] cost 페이지 마크업 및 라우터 설정

--- src/app/cost/page.tsx
+++ src/app/cost/page.tsx
@@ -1,25 +1,49 @@
+"use client";
+
 import { costMenuList } from "@/mock/cost-menu-list";
+import { CostMenu } from "@/types/cost-menu";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
+
+  const handleMenuClick = (id: string) => {
+    router.push(`/cost/detail/${id}`);
+  };
+
   return (
     <div className="px-4 md:w-full max-w-[1100px] mx-auto flex flex-col pt-7 pb-20">
-      <span className="text-dark2 text-3xl mb-5">Menu List</span>
+      <span className="text-dark2 text-3xl mb-5">MENU COST</span>
       <div className="overflow-x-auto">
         <table className="min-w-full text-sm text-left text-dark2">
           <thead className="bg-gray-300">
             <tr>
-              <th className="px-4 py-2 font-medium text-dark2 border-x border-gray-200">메뉴명</th>
-              <th className="px-4 py-2 font-medium text-dark2 border-x border-gray-200">원가</th>
-              <th className="px-4 py-2 font-medium text-dark2 border-x border-gray-200">판매가</th>
-              <th className="px-4 py-2 font-medium text-dark2 border-x border-gray-200">마진율</th>
+              <th className="px-4 py-2 font-bold text-dark2 border-x border-gray-200">
+                메뉴명
+              </th>
+              <th className="px-4 py-2 font-bold text-dark2 border-x border-gray-200">
+                원가
+              </th>
+              <th className="px-4 py-2 font-bold text-dark2 border-x border-gray-200">
+                판매가
+              </th>
+              <th className="px-4 py-2 font-bold text-dark2 border-x border-gray-200">
+                마진율
+              </th>
             </tr>
           </thead>
           <tbody>
-            {costMenuList.map((item, index) => (
-              <tr key={index} className="border-b border-gray-200 hover:bg-gray-50">
-                <td className="px-4 py-3 border-x border-gray-200 break-words max-w-[110px]">{item.name}</td>
-                <td className="px-4 py-3 border-x border-gray-200">{item.cost}</td>
-                <td className="px-4 py-3 border-x border-gray-200">{item.price}</td>
+            {costMenuList.map((item: CostMenu, index: number) => (
+              <tr
+                key={index}
+                className="border-b border-gray-200 hover:bg-gray-100 hover:cursor-pointer "
+                onClick={() => handleMenuClick(item.id)}
+              >
+                <td className="px-4 py-3 border-x border-gray-200 break-words max-w-[110px] hover:underline hover:text-blue-600">
+                  {item.name}
+                </td>
+                <td className="px-4 py-3 border-x border-gray-200">₩{item.cost}</td>
+                <td className="px-4 py-3 border-x border-gray-200">₩{item.price}</td>
                 <td className="px-4 py-3 border-x border-gray-200">{item.margin}</td>
               </tr>
             ))}
---
+++ src/types/cost-menu.ts
@@ -0,0 +1,7 @@
+export interface CostMenu {
+  id: string;
+  name: string;     
+  cost: string; 
+  price: string;   
+  margin: string; 
+}
