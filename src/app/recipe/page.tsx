cbc721bcc2e0b6eba496ec4543f404e68b56a8d4
황혜인 <hhi9037@naver.com>
2024-12-12 21:26:56 +0900

    [feat] recipe 페이지 생성

---
+++ src/app/recipe/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Page() {
+  return (
+    <div className="px-4 md:w-full max-w-[1100px] mx-auto flex flex-col pt-7 pb-20">
+      <span className="text-dark2 text-3xl mb-10">RECIPE</span>
+    
+    </div>
+  );
+}
