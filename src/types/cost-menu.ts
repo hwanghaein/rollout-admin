f325adab6d00e485c91da601883b937944fbd633
황혜인 <hhi9037@naver.com>
2024-12-13 15:38:34 +0900

    [feat] cost-menu 인터페이스 수정

--- src/types/cost-menu.ts
+++ src/types/cost-menu.ts
@@ -1,7 +1,21 @@
+export interface Ingredient {
+  name: string; // 재료명
+  purchasePrice: number; // 구매가 (원)
+  purchaseQuantity: number; // 구매량 (g/ml)
+  usageQuantity: number; // 사용량 (g/ml)
+}
+
 export interface CostMenu {
-  id: string;
-  name: string;     
-  cost: string; 
-  price: string;   
-  margin: string; 
+  id: string; // 메뉴의 고유 ID
+  name: string; // 메뉴 이름
+
+  pricePerPiece: number; // 개당 판매가 (원)
+  salesQuantity: number; // 판매 개수 (개)
+
+  ingredients: Ingredient[]; // 재료 정보 배열
+
+  totalCost: number; // 총 원가 (원)
+  costPerPiece: number; // 개당 원가 (원)
+  margin: number; // 마진율 (%)
+  profitPerPiece: number; // 개당 수익 (원)
 }
