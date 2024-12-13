4c715d87de448fe0da4fe8cfedb0ef9c2f78f1a8
황혜인 <hhi9037@naver.com>
2024-12-13 17:17:54 +0900

    [feat] cost 데이터 패칭 및 파이어베이스 데이터 적용

---
+++ src/app/cost/cost-client.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { CostMenu } from "@/types/cost-menu";
+import { useRouter } from "next/navigation";
+
+interface CostClientProps {
+  costMenuList: CostMenu[];
+}
+
+export default function CostClient({ costMenuList }: CostClientProps) {
+  const router = useRouter();
+
+  const handleClick = (id: string) => {
+    router.push(`/cost/detail/${id}`);
+  };
+
+  console.log(costMenuList);
+  
+
+  return (
+    <table>
+      <tbody>
+        {costMenuList.map((item) => (
+          <tr
+            key={item.id}
+            className="border-b border-gray-200 hover:bg-gray-100 hover:cursor-pointer"
+            onClick={() => handleClick(item.id)}
+          >
+            <td className="px-4 py-3 border-x border-gray-200 break-words max-w-[115px] hover:underline hover:text-blue-600">
+              {item.name}
+            </td>
+            <td className="px-3 py-2 border-x border-gray-200">₩{item.costPerPiece}</td>
+            <td className="px-3 py-2 border-x border-gray-200">₩{item.pricePerPiece}</td>
+            <td className="px-3 py-2 border-x border-gray-200">{item.margin}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
--- src/app/cost/page.tsx
+++ src/app/cost/page.tsx
@@ -1,17 +1,12 @@
-"use client";
+// import { costMenuList } from "@/mock/cost-menu-list";
+import { CostMenu } from '@/types/cost-menu';
+import CostClient from './cost-client';
+import fetchCostMenuItems from "@/utils/fetchCostMenuItems";
 
-import { costMenuList } from "@/mock/cost-menu-list";
-import { CostMenu } from "@/types/cost-menu";
-import { useRouter } from "next/navigation";
+export default async function Page() {
+const costMenuList: CostMenu[] = await fetchCostMenuItems();
 
-export default function Page() {
-  const router = useRouter();
-
-  const handleMenuClick = (id: string) => {
-    router.push(`/cost/detail/${id}`);
-  };
-
-  return (
+ return (
     <div className="px-4 md:w-full max-w-[1100px] mx-auto flex flex-col pt-7 pb-20">
       <span className="text-dark2 text-2xl mb-5">MENU COST</span>
       <div className="overflow-x-auto">
@@ -33,20 +28,7 @@ export default function Page() {
             </tr>
           </thead>
           <tbody>
-            {costMenuList.map((item: CostMenu, index: number) => (
-              <tr
-                key={index}
-                className="border-b border-gray-200 hover:bg-gray-100 hover:cursor-pointer "
-                onClick={() => handleMenuClick(item.id)}
-              >
-                <td className="px-4 py-3 border-x border-gray-200 break-words max-w-[115px] hover:underline hover:text-blue-600">
-                  {item.name}
-                </td>
-                <td className="px-3 py-2 border-x border-gray-200">₩{item.costPerPiece}</td>
-                <td className="px-3 py-2 border-x border-gray-200">₩{item.pricePerPiece}</td>
-                <td className="px-3 py-2 border-x border-gray-200">{item.margin}</td>
-              </tr>
-            ))}
+              <CostClient costMenuList={costMenuList} /> 
           </tbody>
         </table>
       </div>
