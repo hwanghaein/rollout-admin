b52a140aa4134fbd66ffd76af2d264988f25060b
황혜인 <hhi9037@naver.com>
2024-12-19 16:44:46 +0900

    [style] cost 페이지에 다크모드 적용

--- src/app/cost/page.tsx
+++ src/app/cost/page.tsx
@@ -3,7 +3,7 @@ import fetchCostIngredients from "@/utils/fetchCostIngredients";
 import { CostMenu } from "@/types/cost-menu";
 import { CostIngredient } from "@/types/cost-ingredient";
 import ContentSwitcher from "./content-switcher";
-import AuthenticatedRoute from '../../components/authenticated-route';
+import AuthenticatedRoute from "../../components/authenticated-route";
 
 export default async function Page() {
   const costMenuList: CostMenu[] = await fetchCostMenuItems();
@@ -11,13 +11,13 @@ export default async function Page() {
 
   return (
     <AuthenticatedRoute>
-    <div className="px-4 md:w-full max-w-[1100px] mx-auto flex flex-col pt-7 pb-20">
-      <span className="text-dark2 text-xl mb-5">MENU COST</span>
-      <ContentSwitcher
-        costMenuList={costMenuList}
-        costIngredients={costIngredients}
-      />
-    </div>
+      <div className="px-4 md:w-full max-w-[1100px] mx-auto flex flex-col pt-7 pb-20 bg-white dark:bg-gray-900 text-black dark:text-white">
+        <span className="text-dark2 dark:text-gray-200 text-xl mb-5">MENU COST</span>
+        <ContentSwitcher
+          costMenuList={costMenuList}
+          costIngredients={costIngredients}
+        />
+      </div>
     </AuthenticatedRoute>
   );
 }
